b0b923923d3900ff84983f11d26963237904635d
davidbalogh420 <dave.balogh02@gmail.com>
2023-04-16 23:11:15 +0200

    utolagos commit, master branchrol

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainUser.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainUser.java
@@ -2,6 +2,9 @@ package hu.bme.mit.train.interfaces;
 
 public interface TrainUser {
 
+
+	void setAlarmState(boolean alarmState);
+	boolean getAlarmState();
 	int getJoystickPosition();
 
 	boolean getAlarmFlag();
